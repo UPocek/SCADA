adb447cac0cdaadb49e5bae727f47ca43397ea5d
Uros Pocek <uros.pocek@gmail.com>
2023-06-22 16:31:00 +0200

    refactor: Code refactoring

--- scada_front/pages/index.js
+++ scada_front/pages/index.js
@@ -21,7 +21,8 @@ export default function Home() {
 
   useEffect(() => {
     localStorage.getItem('user') == null && router.replace('/login');
-    axios.get(`${baseUrl}/User/userTagsInfo/${getUserId()}`).then(response => updateUserInfo(response.data)).catch(err => console.log("Error on user tags info")); x
+    axios.get(`${baseUrl}/User/userTagsInfo/${getUserId()}`).then(response => updateUserInfo(response.data)).catch(err => console.log("Error on user tags info"));
+    axios.get(`${baseUrl}/Tags/controls`).then(response => setControls(response.data)).catch(err => console.log("Error on controls addresses"));
     const newConnectionTags = new HubConnectionBuilder()
       .withUrl('https://localhost:7214/hubs/tags')
       .withAutomaticReconnect()
